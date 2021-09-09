54cafc067b59e83c6a0c169a120f683ed9e11904
硬頸客 <87679023+hardneck@users.noreply.github.com>
2021-09-09 16:28:40 +0800

    Update test.py
    Let the source code size be trimmed down a bit by eliminating some unnecessary code.

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import csv
+import matplotlib.pyplot as pt
+import numpy as np
+from scipy.interpolate import make_interp_spline
 
 with open('06-51-1452-20-1.txt') as infile:
 	rows = csv.reader(infile)
@@ -14,31 +17,22 @@ with open('06-51-1451-10-8.txt') as infile:
 		a2.append((int(row[0])-100)*12 + int(row[1]))
 		b2.append(int(row[3]))
 
-import matplotlib.pyplot as pt
-import numpy as np
-from scipy.interpolate import make_interp_spline
-
 a1.reverse()
 b1.reverse()
 a2.reverse()
 b2.reverse()
 
-x1 = np.array(a1)
-y1 = np.array(b1)
-x2 = np.array(a2)
-y2 = np.array(b2)
-
-s1 = np.linspace(x1.min(), x1.max(), 200)
-t1 = make_interp_spline(x1, y1, k=3)(s1)
-s2 = np.linspace(x2.min(), x2.max(), 200)
-t2 = make_interp_spline(x2, y2, k=3)(s2)
+x1 = np.linspace(min(a1), max(a1), 200)
+y1 = make_interp_spline(a1, b1, k=3)(x1)
+x2 = np.linspace(min(a2), max(a2), 200)
+y2 = make_interp_spline(a2, b2, k=3)(x2)
 
 pt.title("2011–2014 Household Power Consumption")
 pt.xlabel("Count of Elapsed Months")
 pt.ylabel("Units of Consumed Electricity")
-pt.plot(s1, t1, 'k--')
-pt.plot(s2, t2, 'm--')
-pt.plot(x1, y1, 'yo')
-pt.plot(x2, y2, 'co')
+pt.plot(x1, y1, 'k--')
+pt.plot(x2, y2, 'm--')
+pt.plot(a1, b1, 'yo')
+pt.plot(a2, b2, 'co')
 pt.legend(["128", "130"], loc="upper center")
 pt.show()
