af7602346cfc4dd2e98a138d40598c534288baab
硬頸客 <87679023+hardneck@users.noreply.github.com>
2021-09-10 09:22:05 +0800

    更新主要且唯一的程式
    在圖例函數中使用不同的引數格式，以及添加三條中文註解。

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as pt
 import numpy as np
 from scipy.interpolate import make_interp_spline
 
+# 輸入成功路128號之電費帳單資料，共計25筆。
 with open('06-51-1452-20-1.txt') as infile:
 	rows = csv.reader(infile)
 	a1, b1 = [], []
@@ -10,6 +11,7 @@ with open('06-51-1452-20-1.txt') as infile:
 		a1.append((int(row[0])-100)*12 + int(row[1]))
 		b1.append(int(row[3]))
 
+# 輸入成功路130號之電費帳單資料，共計25筆。
 with open('06-51-1451-10-8.txt') as infile:
 	rows = csv.reader(infile)
 	a2, b2 = [], []
@@ -17,6 +19,7 @@ with open('06-51-1451-10-8.txt') as infile:
 		a2.append((int(row[0])-100)*12 + int(row[1]))
 		b2.append(int(row[3]))
 
+# 須將橫軸資料由小至大排列，而對應之縱軸資料亦隨之改變。
 a1.reverse()
 b1.reverse()
 a2.reverse()
@@ -29,10 +32,10 @@ y2 = make_interp_spline(a2, b2, k=3)(x2)
 
 pt.title("2011–2014 Household Power Consumption")
 pt.xlabel("Count of Elapsed Months")
-pt.ylabel("Units of Consumed Electricity")
+pt.ylabel("Units of Electricity Consumed")
 pt.plot(x1, y1, 'k--', label="_nolegend_")
 pt.plot(x2, y2, 'k--', label="_nolegend_")
-pt.plot(a1, b1, 'yo')
-pt.plot(a2, b2, 'co')
-pt.legend(["128", "130"], loc="upper center")
+pt.plot(a1, b1, 'yo', label="128")
+pt.plot(a2, b2, 'co', label="130")
+pt.legend(loc="upper center")
 pt.show()
